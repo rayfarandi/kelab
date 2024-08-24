e32092b830256742817dd8b9fab860fc43c1723d
rayfrandi <rayfrandi1994@gmail.com>
2024-08-25 01:15:10 +0700

    update

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,6 +6,6 @@ const roleCheckMiddleware = require('../middleware/roleCheck.middleware')
 
 
 router.use('/auth', require('./auth.router'))
-router.use('/admin', authMiddleware, roleCheckMiddleware("admin"), require('./admin'))                            // setiap ada permintaan ke path /admin authMiddleware dan roleCheckMiddleware akan selalu dijalankan untuk pengecekan otorisasi
+router.use('/admin', authMiddleware, roleCheckMiddleware("admin"), require('./admin'))// setiap ada permintaan ke path /admin authMiddleware dan roleCheckMiddleware akan selalu dijalankan untuk pengecekan otorisasi
 
 module.exports = router
